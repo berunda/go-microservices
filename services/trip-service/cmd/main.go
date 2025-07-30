17a4e7598b0f565a87bfe51b7eee3c828af71e16
berunda <jonberunda@gmail.com>
2025-07-30 06:25:57 -0400

    Gracefull shutdown

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
 	"ride-sharing/shared/env"
+	"syscall"
+	"time"
 )
 
 var (
@@ -31,7 +36,26 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("Error starting server: %v", err)
+	serverErrors := make(chan error, 1)
+	go func() {
+		log.Printf("Listening on %s", httpAddr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Printf("Error starting the server: %v", err)
+	case sig := <-shutdown:
+		log.Printf("Received signal: %s, shutting down", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("Could not stop server gracefully: %v", err)
+			server.Close()
+		}
 	}
 }
